8cfda726536198584f5a8c42fe4500129d9d2420
Freyr Þorvaldsson <freyrthorv@gmail.com>
2021-01-19 13:37:19 +0000

    Generate log entires when server is running
    This is to demonstrate the polling functionality.

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var fs = require("fs");
 
 var indexRouter = require("./routes/index");
 var logsRouter = require("./routes/logs");
@@ -29,3 +30,38 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
+
+// This awkwardly placed code makes logs every n seconds.
+// This is to demonstrate how the front end can handle new data.
+const interval = 2;
+console.log(`Making logs every ${interval} seconds`);
+const logFilePath = path.resolve(__dirname, "log-file.txt");
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
+
+setInterval(async () => {
+	const datetime = new Date();
+	const formattedDatetime = datetime
+		.toISOString()
+		.substr(0, 23)
+		.replace(".", ",")
+		.replace("T", " ");
+
+	const data = [
+		`ERROR Some error message`,
+		`WARNING Some warning message of reversed`,
+		`WARNING Some warning message of reversed`,
+		`INFO Some info message`,
+		`INFO Some info message`,
+		`INFO Some info message`,
+		`INFO Some info message`,
+		`INFO Some info message`,
+	];
+
+	fs.appendFile(logFilePath, `\n${formattedDatetime} ${data[getRandomInt(8)]}`, function (err) {
+		if (err) throw err;
+		console.log("1 new log entry made");
+	});
+}, interval * 1000);
