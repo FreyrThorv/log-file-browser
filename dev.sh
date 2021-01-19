b5f184e9add8fc79cd4f0b8ebcac803c4841c544
Freyr Þorvaldsson <freyrthorv@gmail.com>
2021-01-19 13:35:43 +0000

    Fix order in which commands are run

--- dev.sh
+++ dev.sh
@@ -1,2 +1,4 @@
 # Runs frontend and backend simultaniously, in the same terminal.
-cd ./frontend npm run start && nodemon ./backend/bin/www
+nodemon ./backend/bin/www &
+cd ./frontend 
+npm run start
